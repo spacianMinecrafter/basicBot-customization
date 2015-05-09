74cfd7d321eddd6d67e79f10f3596b169e966934
spacianMinecrafter <diana.lee.holmes@gmail.com>
2015-05-08 18:12:27 -0600

    No idea what I'm doing
    If I did this incorrectly I apologize and ask you to pay it no attention; I really have no idea what I'm doing.

--- extension.js
+++ extension.js
@@ -33,6 +33,48 @@
          }
 
          */
+         
+    },
+        eventUserjoin: function (user) {
+            var known = false;
+            var index = null;
+            for (var i = 0; i < basicBot.room.users.length; i++) {
+                if (basicBot.room.users[i].id === user.id) {
+                    known = true;
+                    index = i;
+                }
+            }
+            var greet = true;
+            var welcomeback = null;
+            if (known) {
+                basicBot.room.users[index].inRoom = true;
+                var u = basicBot.userUtilities.lookupUser(user.id);
+                var jt = u.jointime;
+                var t = Date.now() - jt;
+                if (t < 10 * 1000) greet = false;
+                else welcomeback = true;
+            }
+            else {
+                basicBot.room.users.push(new basicBot.User(user.id, user.username));
+                welcomeback = false;
+            }
+            for (var j = 0; j < basicBot.room.users.length; j++) {
+                if (basicBot.userUtilities.getUser(basicBot.room.users[j]).id === user.id) {
+                    basicBot.userUtilities.setLastActivity(basicBot.room.users[j]);
+                    basicBot.room.users[j].jointime = Date.now();
+                }
+
+            }
+            if (basicBot.settings.welcome && greet) {
+                welcomeback ?
+                    setTimeout(function (user) {
+                        API.sendChat(subChat(basicBot.chat.welcomeback, {name: user.username}));
+                    }, 1 * 1000, user)
+                    :
+                    setTimeout(function (user) {
+                        API.sendChat(subChat(basicBot.chat.Awild {name: user.username}) hasappeared!);
+                    }, 1 * 1000, user);
+            }
 
         bot.commands.baconCommand = {
             command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
